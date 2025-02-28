996e45bcac5ac9c64ee734fb85516ce7fa9d39db
Igor633 <202501706363@alunos.estacio.br>
2025-02-27 21:27:52 -0300

    baralho

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,8 +1,47 @@
-
-
 #include <stdio.h>
 
 int main() {
-	printf("Super Trunfo!\n");
+    // Declaração das variáveis para armazenar os dados das cartas
+    int populacao1, populacao2;
+    float area1, area2, pib1, pib2;
+    int pontos_turisticos1, pontos_turisticos2;
+
+    // Entrada de dados para a primeira carta
+    printf("Digite os dados da primeira carta:\n");
+    printf("População: ");
+    scanf("%d", &populacao1);
+    printf("Área: ");
+    scanf("%f", &area1);
+    printf("PIB: ");
+    scanf("%f", &pib1);
+    printf("Número de pontos turísticos: ");
+    scanf("%d", &pontos_turisticos1);
+
+    // Entrada de dados para a segunda carta
+    printf("\nDigite os dados da segunda carta:\n");
+    printf("População: ");
+    scanf("%d", &populacao2);
+    printf("Área: ");
+    scanf("%f", &area2);
+    printf("PIB: ");
+    scanf("%f", &pib2);
+    printf("Número de pontos turísticos: ");
+    scanf("%d", &pontos_turisticos2);
+
+    // Exibição dos dados da primeira carta
+    printf("\nDados da primeira carta:\n");
+    printf("População: %d\n", populacao1);
+    printf("Área: %.2f\n", area1);
+    printf("PIB: %.2f\n", pib1);
+    printf("Número de pontos turísticos: %d\n", pontos_turisticos1);
+
+    // Exibição dos dados da segunda carta
+    printf("\nDados da segunda carta:\n");
+    printf("População: %d\n", populacao2);
+    printf("Área: %.2f\n", area2);
+    printf("PIB: %.2f\n", pib2);
+    printf("Número de pontos turísticos: %d\n", pontos_turisticos2);
+
     return 0;
 }
+
